0b58c04e8d7d5c099570e4da00999756427d2b51
Iftixor Turamuradov <Iftixort5@outlook.com>
2022-11-22 01:17:27 +0500

    I change code

---
+++ first_lesson.py
@@ -0,0 +1,58 @@
+# ism = "Iftikhor"
+# familya = "Turamuradov"
+# age = 21
+# # Bu print hossasi malumotlarni chiqarish uchun ishlatilinadi
+# print(ism, familya, age)
+
+
+set = 5
+
+print(set)
+print(type(set))
+print('------------------')
+
+floating = 5.6
+print(floating)
+print(type(floating))
+print('------------------')
+
+name = 'Iftixor'
+print(name)
+print(type(name))
+print('------------------')
+
+list = [5,"iftikhor", 5.4]
+print(list)
+print(type(list))
+print('------------------')
+
+dic = {"book": "kitob", "name": "Iftikor"}
+print(dic)
+print(type(dic))
+print('------------------')
+
+tuples  = (4,7.5, "name")
+print(tuples)
+print(type(tuples))
+print('------------------')
+
+
+alphabet = {"a", "b", "c"}
+print(alphabet)
+print(type(alphabet))
+print('------------------')
+
+a = 25
+b = 48
+
+c = a!=b
+
+print(c)
+print(type(c))
+
+
+
+
+
+
+
