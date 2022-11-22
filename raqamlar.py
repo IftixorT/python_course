4b52f1c794af44ae8ac4979feab34ab43fd1ec5f
Iftixor Turamuradov <Iftixort5@outlook.com>
2022-11-23 01:48:09 +0500

    I create raqamlar file

---
+++ raqamlar.py
@@ -0,0 +1,14 @@
+butun_son = 10
+
+print(butun_son)
+print(type(butun_son))
+
+float_son = float(butun_son)
+print(float_son)
+print('-----------------------')
+
+print('-----------Tasodifiy son------------')
+import random
+
+print(random.randrange(1, 100 ))
+
